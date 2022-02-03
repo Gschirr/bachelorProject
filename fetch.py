4465950480bf501d067028a3f3592ddd6b605329
Gschirr <s.gschirr@gmail.com>
2022-02-03 18:05:40 +0100

    reworked code

--- fetch.py
+++ fetch.py
@@ -1,38 +1,83 @@
-import requests
+import os
+
+import pandas as pd
+import datetime as dt
+import time
+import praw
+from psaw import PushshiftAPI
+# to use PSAW
 import loginCredentialsConfig
 
+api = PushshiftAPI()
+# to use PRAW
 
-credentials = loginCredentialsConfig.Credentials
+logCred = loginCredentialsConfig.Credentials
+# create praw instance
+reddit = praw.Reddit(
+    client_id       = logCred.clientID,
+    client_secret   = logCred.secretToken,
+    username        = logCred.username,
+    password        = logCred.password,
+    user_agent      = logCred.userAgent
+)
 
-# note that CLIENT_ID refers to 'personal use script' and SECRET_TOKEN to 'token'
-auth = requests.auth.HTTPBasicAuth(credentials.clientID, credentials.secretToken)
+# gets all comments from one submission
+subreddit = reddit.subreddit("android")
 
-# here we pass our login method (password), username, and password
-# hide with config file that is not public
-data = {'grant_type': 'password',
-        'username': credentials.username,
-        'password': credentials.password}
+# collect submission authors name, id and submission id
+dataSetSubmission = {
+    "author": [],
+    "author_Id": [],
+    "submission_Id": []
+    # "comment": []
+}
 
-# setup our header info, which gives reddit a brief description of our app
-headers = {'User-Agent': 'myProjectBot/test'}
-
-# send our request for an OAuth token
-res = requests.post('https://www.reddit.com/api/v1/access_token',
-                    auth=auth, data=data, headers=headers)
-
-# convert response to JSON and pull access_token value
-TOKEN = res.json()['access_token']
-
-# add authorization to our headers dictionary
-headers = {**headers, **{'Authorization': f"bearer {TOKEN}"}}
-
-# while the token is valid (~2 hours) we just add headers=headers to our requests
-requests.get('https://oauth.reddit.com/api/v1/me', headers=headers)
+commentsList = []
+# outer loop fetches all the submissions
+start_time = time.time()
+for submission in subreddit.hot(limit=100):
+    if submission.pinned == True or submission.stickied == True:
+        continue
+    # print(40*"Xx")
+    # print("Submission Author: {}, Submission comment {}".format(submission.author, submission.selftext))
+    # print()
+    dataSetSubmission["author"].append(submission.author)
+    dataSetSubmission["author_Id"].append(submission.author.id)
+    dataSetSubmission["submission_Id"].append(submission.id)
+    # dataSetSubmission["comment"].append(submission.selftext)
 
 
-res = requests.get("https://oauth.reddit.com/r/python/hot", headers=headers)
+    submission.comments.replace_more(limit=10)
+    # inner loop fetches corresponding comments to submission
+    for comment in submission.comments.list():
+        if comment == None or comment.author == None or comment.author.id == None:
+            continue
+        dataSetUserInSubmission = {}
+        dataSetUserInSubmission["parent_submission"] = submission
+        dataSetUserInSubmission["parent_submission_Id"] = submission.id
+        dataSetUserInSubmission["author"] = comment.author
+        # dataSetUserInSubmission["author_Id"] = comment.author.id
+        dataSetUserInSubmission["comment"] = comment.body
+        # dataSetUserInSubmission["comment_Id"] = comment.id
 
-# print(res.json())
+        print(20*"/")
+        print("Comment Author: {}, comment: \n{}".format(comment.author, comment.body))
+        print()
+        commentsList.append(dataSetUserInSubmission)
+        # dataFrameComment = pd.DataFrame(dataSetUserInSubmission, index=[comment.author.id])
+        # print(dataFrameComment)
 
-for post in res.json()['data']['children']:
-        print(post['data']['title'] + "\n")
+# create dataframe for every User with his corresponding posts to android is goal
+dataFrameComment = pd.DataFrame(commentsList)
+dataFrameSubmission = pd.DataFrame(dataSetSubmission)
+
+print(dataFrameSubmission)
+action = f"\t\t[Info] Elapsed time: {time.time() - start_time: .2f}s"
+print(action)
+# submissions = reddit.submission(id="sfgq5i")
+
+# submission.comments.replace_more(limit=None)
+# for comment in submission.comments.list():
+#     print("Author: {}, comment: \n{}".format(comment.author, comment.body))
+
+# df.to_csv("topPostsAndroid.csv", header=False, encoding="utf-8", index=False)
---
+++ fetch_old.py
@@ -0,0 +1,38 @@
+import requests
+import loginCredentialsConfig
+
+
+credentials = loginCredentialsConfig.Credentials
+
+# note that CLIENT_ID refers to 'personal use script' and SECRET_TOKEN to 'token'
+auth = requests.auth.HTTPBasicAuth(credentials.clientID, credentials.secretToken)
+
+# here we pass our login method (password), username, and password
+# hide with config file that is not public
+data = {'grant_type': 'password',
+        'username': credentials.username,
+        'password': credentials.password}
+
+# setup our header info, which gives reddit a brief description of our app
+headers = {'User-Agent': 'myProjectBot/test'}
+
+# send our request for an OAuth token
+res = requests.post('https://www.reddit.com/api/v1/access_token',
+                    auth=auth, data=data, headers=headers)
+
+# convert response to JSON and pull access_token value
+TOKEN = res.json()['access_token']
+
+# add authorization to our headers dictionary
+headers = {**headers, **{'Authorization': f"bearer {TOKEN}"}}
+
+# while the token is valid (~2 hours) we just add headers=headers to our requests
+requests.get('https://oauth.reddit.com/api/v1/me', headers=headers)
+
+
+res = requests.get("https://oauth.reddit.com/r/android/hot/comments", headers=headers)
+
+print(res.json())
+
+# for post in res.json()['data']['children']:
+#         print(post['data']['title'] + "\n")
